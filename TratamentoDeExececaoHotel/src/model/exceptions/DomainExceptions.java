a79e2078de52b8600233988f232313005c5c143b
LucasLomas <lucaslomas5452@gmail.com>
2023-01-23 21:57:51 -0300

    primeira versao

--- TratamentoDeExececaoHotel/src/model/exceptions/DomainExceptions.java
+++ TratamentoDeExececaoHotel/src/model/exceptions/DomainExceptions.java
@@ -1,2 +1,7 @@
-package model.exceptions;public class DomainExceptions {
+package model.exceptions;
+
+public class DomainExceptions extends RuntimeException{
+    public DomainExceptions(String msg){
+        super(msg);
+    }
 }
